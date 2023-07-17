b06618ae1638229da56ce7e6f1a74b25c55baf61
sofiaparraweb <sofiaparraweb@gmail.com>
2023-07-17 14:43:04 -0400

    redux

---
+++ src/Redux/actions.tsx
@@ -0,0 +1,21 @@
+// import { User } from "@auth0/auth0-spa-js";
+// import axios from "axios";
+
+// export const GET_ALL_ACTIVITY = "GET_ALL_ACTIVITY";
+
+// export const url = "http://localhost:3001";
+
+// export function getAllActivity() {
+//   return async function (dispatch) {
+//     try {
+//       const res = await axios.get();
+//       return dispatch({
+//         type: ,
+//         payload:,
+//       });
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
+// }
+
