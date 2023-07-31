6e6d53efa7bf0a9e470f0c2ac1dccd0f8cb68e62
Pedro Fede Casillas <pedrofedecasillas2@gmail.com>
2023-07-31 11:18:23 -0300

    reviews modificados funcionando junto con el back

--- src/views/Detail/Stars.jsx
+++ src/views/Detail/Stars.jsx
@@ -3,42 +3,31 @@ import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 import { AiOutlineStar } from "react-icons/ai";
 import "./Stars.css";
 
-const Stars = ({ stars, reviews, onStarClick }) => {
-  const [hoveredStar, setHoveredStar] = useState(0);
-
-  const handleStarClick = (selectedStar) => {  
-    onStarClick(selectedStar);
-  };
+const Stars = ({stars, reviews}) =>{
 
   const ratingStar = Array.from({ length: 5 }, (elem, index) => {
-    let number = index + 0.5;
-    return (
-      <span
-        key={index}
-        onClick={() => handleStarClick(index + 1)}
-        onMouseEnter={() => setHoveredStar(index + 1)}
-        onMouseLeave={() => setHoveredStar(0)}
-      >
-        {stars >= index + 1 ? (
-          <FaStar className="iconStars" />
-        ) : stars >= number ? (
-          <FaStarHalfAlt className="iconStars" />
-        ) : hoveredStar >= index + 1 ? (
-          <FaStar className="iconStars" />
-        ) : (
-          <AiOutlineStar className="iconStars" />
-        )}
-      </span>
-    );
+      let number = index + 0.5;
+
+      return (
+        <span key={index}>
+          {stars >= index + 1 ? (
+            <FaStar className="iconStars" />
+          ) : stars >= number ? (
+            <FaStarHalfAlt className="iconStars" />
+          ) : (
+            <AiOutlineStar className="iconStars" />
+          )}
+        </span>
+      );
   });
 
   return (
-    <div className="">
-      <div className="icon-style">
-        {ratingStar}
-        <p style={{ color: "grey" }}>({reviews} customer reviews)</p>
+      <div className="">
+          <div className="icon-style">
+              {ratingStar}
+              <p style={{color:"grey"}}>({reviews} customer reviews)</p>
+          </div>
       </div>
-    </div>
   );
 };
 
--- src/views/reviewForm/ReviewForm.jsx
+++ src/views/reviewForm/ReviewForm.jsx
@@ -1,74 +1,98 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import Stars from '../Detail/Stars';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { addReview } from '../../Redux/actions';
 import "./ReviewForm.css"
 
-const ReviewForm = ({ bookId }) => {
+const ReviewForm = ({ id }) => {
   const dispatch = useDispatch();
-  const [rating, setRating] = useState(0);
-  const [comment, setComment] = useState('');
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  // const [rating, setRating] = useState(0);
+  // const [comment, setComment] = useState('');
+  // const [isSubmitting, setIsSubmitting] = useState(false);
+  const user_id = useSelector(state => state.LocalPersist.userInfo.email);
 
-  const handleRatingChange = (newRating) => {
-    setRating(newRating);
-  };
-
+  const [review, setReview] = useState({  // --------------------------------------------------REVIEWS
+    email:`${user_id}`, /* <----------------------- FALTA ASIGNARLE BIEN EL USERID QUE TIENE EL USUARIO QUE COMENTA */
+    content: "",
+    rating: 0,
+    book_id: `${id}`, 
+  })
+  
   const handleCommentChange = (event) => {
-    setComment(event.target.value);
-  };
+    const property = event.target.name;
+    const value = event.target.value;
+    setReview({ ...review, [property]: value});
+  }
 
-  const handleSubmitReview = async (event) => {
-    event.preventDefault();
-
-    if (rating === 0) {
-        alert("Por favor, califica al menos una estrella antes de enviar tu reseña.");
-        return;
-      }
-
-      if (comment.trim().split(/\s+/).length < 2) {
-        alert("Por favor, ingresa al menos dos palabras en tu reseña.");
-        return;
-      }
-
-    // if (rating === 0 || comment.trim() === '') {
-     
-    //   return;
-    // }
-    setIsSubmitting(true);
+  const url =  "http://localhost:3001";
+  const handleSubmit = async (event) => {
+    event.preventDefault()
     try {
-      const reviewData = {
-        user_id: bookId, 
-        content: comment, 
-        rating: rating, 
-        product_id: bookId, 
-      };
-      await dispatch(addReview(reviewData));
-
+      await axios.post(`${url}/review/post`, review)
+      .then(res=>alert("Gracias por opinar sobre nuestro producto!"))
+      .catch((error) => alert(error))
     } catch (error) {
-      console.error('Error creating review:', error);
-    } finally {
-      setIsSubmitting(false);
+      console.log(error);
     }
-  };
+  } 
+
+  // const handleSubmitReview = async (event) => {
+  //   event.preventDefault();
+
+  //   if (review.rating === 0) {
+  //       alert("Por favor, califica al menos una estrella antes de enviar tu reseña.");
+  //       return;
+  //     }
+
+  //     if (review.content.trim().split(/\s+/).length < 2) {
+  //       alert("Por favor, ingresa al menos dos palabras en tu reseña.");
+  //       return;
+  //     }
+
+  //   if (review.rating === 0 || review.content.trim() === '') {
+     
+  //     return;
+  //   }
+  //   setIsSubmitting(true);
+  //   try {
+  //     const reviewData = {
+  //       email: review.email, 
+  //       content: review.content, 
+  //       rating: review.rating, 
+  //       book_id: review.book_id, 
+  //     };
+  //     await dispatch(addReview(reviewData));
+
+  //   } catch (error) {
+  //     console.error('Error creating review:', error);
+  //   } finally {
+  //     setIsSubmitting(false);
+  //   }
+  // };
 
   return (
     <div className="ReviewContainer">
       <h3>Your Review</h3>
       <div className="RatingContainer">
-       
-        <Stars stars={rating} onStarClick={handleRatingChange} />
+      <textarea
+          type="range" min="0" max="5"
+          name="rating"
+          onChange={handleCommentChange}
+          value={review.rating}
+        ></textarea>
+
       </div>
       <div className="CommentContainer">
         <textarea
-          value={comment}
+          value={review.content}
+          name="content"
           onChange={handleCommentChange}
           placeholder="Write your review here..."
         ></textarea>
       </div>
       <div className="SubmitContainer">
-        <button type="submit" onClick={handleSubmitReview} disabled={isSubmitting}>
+        <button type="submit" onClick={handleSubmit} >
           Submit Review
         </button>
       </div>
@@ -77,3 +101,4 @@ const ReviewForm = ({ bookId }) => {
 };
 
 export default ReviewForm;
+
